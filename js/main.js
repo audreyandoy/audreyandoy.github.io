4b9b26742ff3c97042e835050ae12fb3a4cd99ec
audreyandoy <audreylandoy@gmail.com>
2016-04-27 04:11:42 -0700

    added skills section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-    anchors:['firstPage', 'secondPage', 'thirdPage'],
+    anchors:['About', 'Skills', 'Projects'],
+    menu: '#menu',
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['About', 'Skills', 'Projects'],
+    showActiveTooltip: true,
+    sectionsColor : ['#fff', '#000', '#fff', '#000'],
     css3: true
   });
 
