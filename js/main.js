4b4a062ea352908246e520e8935f0396d77a06d5
audreyandoy <audreylandoy@gmail.com>
2017-02-27 04:30:07 -0800

    updated profile

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
     anchors:['About', 'Skills', 'Projects'],
-    menu: '#menu',
+    menu: '#side-nav',
     navigation: true,
     navigationPosition: 'left',
     navigationTooltips: ['About', 'Skills', 'Projects'],
     showActiveTooltip: true,
     sectionsColor : ['#fff', '#000', '#fff', '#000'],
-    scrollOverflow: false
+    scrollOverflow: false,
+
   });
+
+
 
     $(function(){
         $(".element").typed({
-            strings: ["Portfolio In Progress!"],
+            strings: ["Audrey Andoy"],
             typeSpeed: 60,
             contentType: 'html'
         });
