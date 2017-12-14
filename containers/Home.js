80460379eb731d017a320c30fa2353558a3e5937
audreyandoy <audreylandoy@gmail.com>
2017-12-14 08:06:36 -0800

    temp restore

---
+++ containers/Home.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Home extends Component {
+  static PropTypes = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Hi Home</h1>
+      </div>
+    );
+  }
+
+}
